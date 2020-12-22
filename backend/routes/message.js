e8f2e000de45b4226cc98563ae832ea5510ba2cb
Adrien <adrien.lehugeur@gmail.com>
2020-12-23 00:42:30 +0100

    controller message

--- backend/routes/message.js
+++ backend/routes/message.js
@@ -8,8 +8,8 @@ const messageCtrl = require('../controllers/message');
 
 router.post('/', auth, multer, messageCtrl.createMessage);
 router.get('/', auth, messageCtrl.findAllMessages);
-router.get('/:messageId', auth, multer, messageCtrl.findOneMessage);
-router.put('/:messageId', auth, multer, messageCtrl.updateMessage);
-router.delete('/:messageId', auth, messageCtrl.deleteMessage);
+//router.get('/:messageId', auth, multer, messageCtrl.findOneMessage);
+//router.put('/:messageId', auth, multer, messageCtrl.updateMessage);
+//router.delete('/:messageId', auth, messageCtrl.deleteMessage);
 
 module.exports = router; 
